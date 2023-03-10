9494e057f3e7f9a69221b720e40216113d2b55d1
soheib kiani <mrsoheibkiani@gmail.com>
2023-03-10 21:28:54 +0330

    Add Second-Description && Quote Components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Header from '../components/header';
 import Main from '../components/main';
 import FDescription from '../components/first-description';
+import SDescription from '../components/second-sedcription';
+import Quote from '../components/quote';
 export default function Home() {
   return (
     <>
       <Header />
       <Main />
       <FDescription />
+      <SDescription />
+      <Quote />
     </>
   );
 }
