115f504ca4ad90e9c82c9d9a871c9acb9383c8ca
soheib kiani <mrsoheibkiani@gmail.com>
2023-03-10 18:42:43 +0330

    Add First-Description Component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Header from '../components/header';
 import Main from '../components/main';
+import FDescription from '../components/first-description';
 export default function Home() {
   return (
     <>
       <Header />
       <Main />
+      <FDescription />
     </>
   );
 }
